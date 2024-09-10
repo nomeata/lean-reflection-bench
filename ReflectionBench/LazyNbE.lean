52362e4e4b099361dc32598d2427a53a515599b8
Joachim Breitner <mail@joachim-breitner.de>
2024-09-10 13:19:13 +0200

    Instantiate in the right order

--- ReflectionBench/LazyNbE.lean
+++ ReflectionBench/LazyNbE.lean
@@ -15,21 +15,25 @@ unsafe instance : Inhabited Val where
 private def Lean.ConstructorVal.arity (ci : ConstructorVal) : Nat :=
   ci.numParams + ci.numFields
 
+mutual
+unsafe def force (genv : Environment) (lctx : LocalContext) : Val → MetaM Val
+  | .thunk e ρ => do
+    force genv lctx (← eval genv lctx e ρ)
+  | v => return v
+
 unsafe def eval (genv : Environment) (lctx : LocalContext) (e : Expr) (ρ : List Val) :
     MetaM Val := do
   match e with
-  | .bvar n => match ρ[n]! with
-    | .thunk e ρ => eval genv lctx e ρ
-    | v => return v
+  | .bvar n => return ρ[n]!
   | .lam n t b bi =>
     return .closure n t bi (fun x => return .thunk b (x :: ρ))
   | .app e₁ e₂ =>
-      match (← eval genv lctx e₁ ρ) with
+      match (← force genv lctx (← eval genv lctx e₁ ρ)) with
       | .neutral e₁' => return .neutral (.app e₁' e₂)
       | .lit l => throwError "Cannot apply literal {repr l} to {e₂}"
       | .con n _ _ _ _ => throwError "Cannot apply constructor {n} to {e₂}"
       | .closure _ _ _ f => f (.thunk e₂ ρ)
-      | .thunk _ _ => panic! "eval returned thunk"
+      | .thunk _ _ => panic! "force returned thunk"
   | .const n us =>
       let some ci := genv.find? n | throwError "Did not find {n}"
       match ci with
@@ -44,10 +48,11 @@ unsafe def eval (genv : Environment) (lctx : LocalContext) (e : Expr) (ρ : List
       --  return .some (.rec_ ci us #[])
       | _ => return .neutral e
   | _ => throwError "eval: unimplemented: {e}"
+end
 
 unsafe def readback : Val → MetaM Expr
   | .neutral e => return e
-  | .thunk e ρ => return e.instantiateRev (← ρ.mapM readback).toArray
+  | .thunk e ρ => return e.instantiate (← ρ.mapM readback).toArray
   | .lit l => return .lit l
   | .con cn _ _ us args => return mkAppN (.const cn us) (← args.mapM readback)
   | .closure n t bi f =>
@@ -56,7 +61,7 @@ unsafe def readback : Val → MetaM Expr
       Meta.mkLambdaFVars #[x] (← readback r)
 
 unsafe def lazyWhnfImpl (genv : Environment) (lctx : LocalContext) (e : Expr) : MetaM Expr := do
-  readback (← eval genv lctx e [])
+  readback (← force genv lctx (← eval genv lctx e []))
 
 @[implemented_by lazyWhnfImpl]
 opaque lazyWhnf (genv : Environment) (lctx : LocalContext) (e : Expr) : MetaM Expr
@@ -68,6 +73,4 @@ elab "#nbe_reduce" t:term : command => Lean.Elab.Command.runTermElabM fun _ => d
 
 set_option linter.unusedVariables false
 
-#nbe_reduce (fun y => y) (fun y => y)
-#nbe_reduce (fun z => (fun y => y) (fun y => y))
-#nbe_reduce (fun y => (fun z => (fun x => x) (fun x => y))) ((fun a => a) (fun a => false))
+#nbe_reduce true
