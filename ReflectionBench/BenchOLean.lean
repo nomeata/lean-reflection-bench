523cd0a7967fa208c4edbdfba3b7f8a0f86e0035
Joachim Breitner <mail@joachim-breitner.de>
2024-09-13 16:10:22 +0200

    Filter interesting modules, timing

--- ReflectionBench/BenchOLean.lean
+++ ReflectionBench/BenchOLean.lean
@@ -12,6 +12,7 @@ partial def isReflProof (e : Expr) : Bool :=
   | _ => false
 
 def runWhnf (desc : String) (whnf : Environment → LocalContext → Expr → MetaM Expr) (e : Expr) : MetaM Unit:= do
+  let startT ← IO.monoMsNow
   try
     let r ← whnf (← Lean.getEnv) (← Lean.getLCtx) e
     unless r matches .const ``Bool.true [] do
@@ -19,6 +20,10 @@ def runWhnf (desc : String) (whnf : Environment → LocalContext → Expr → Me
   catch ex =>
     let f ← Lean.MessageData.format (Lean.Exception.toMessageData ex)
     IO.println f!"{desc} failed: {f}"
+  let endT ← IO.monoMsNow
+  let diffT := endT - startT
+  if diffT > 3 then
+    IO.println s!"{desc}: {endT - startT}ms"
 
 def kernelWhnf (env : Environment) (lctx : LocalContext) (e : Expr) : MetaM Expr := do
   Lean.ofExceptKernelException <| Lean.Kernel.whnf env lctx e
@@ -33,11 +38,13 @@ def checkDecide (p inst eq : Expr) : MetaM Unit := do
   else
       IO.println f!"ignoring proof {← ppExpr eq}"
 
-def checkBody (e : Expr) : MetaM Unit := do
-  if Option.isSome <| e.find? fun e =>
+def hasInterestingFunction (e : Expr) : Bool :=
+  Option.isSome <| e.find? fun e =>
        e.isAppOfArity ``of_decide_eq_true 3
     || e.isAppOfArity ``eq_true_of_decide 3
-  then
+
+def checkBody (e : Expr) : MetaM Unit := do
+  if hasInterestingFunction e then
     let _ ← transform e (skipConstInApp := true) fun e => do
       match_expr e with
       | of_decide_eq_true p inst eq => checkDecide p inst eq
@@ -45,7 +52,6 @@ def checkBody (e : Expr) : MetaM Unit := do
       | _ => pure ()
       return .continue
   return
-
 
 def checkConstInfo (ci : ConstantInfo) : MetaM Unit := do
   match ci with
@@ -56,8 +62,7 @@ def checkConstInfo (ci : ConstantInfo) : MetaM Unit := do
 def checkConstInfos (consts : Array ConstantInfo) : MetaM Unit := do
   consts.forM checkConstInfo
 
-unsafe def checkMod (module : Name) : IO Unit := do
-  IO.println s!"Processing {module}"
+unsafe def withMod (module : Name) (k : Array ConstantInfo → MetaM Unit): IO Unit := do
   let mFile ← findOLean module
   unless (← mFile.pathExists) do
     throw <| IO.userError s!"object file '{mFile}' of module {module} does not exist"
@@ -66,7 +71,7 @@ unsafe def checkMod (module : Name) : IO Unit := do
   let env ← finalizeImport s #[{module}] {} 0
   let ctx := {fileName := "mFile", fileMap := Inhabited.default}
   let state := {env}
-  Prod.fst <$> (MetaM.run' (checkConstInfos mod.constants)).toIO ctx state
+  Prod.fst <$> (MetaM.run' (k mod.constants)).toIO ctx state
   env.freeRegions
   region.free
 
@@ -74,17 +79,27 @@ unsafe def main (args : List String) : IO UInt32 := do
   initSearchPath (← findSysroot)
   let sp ← searchPathRef.get
   let (flags, args) := args.partition fun s => s.startsWith "--"
-  match flags, args with
-    | [], args =>
-      args.forM fun arg => do
-        let mod ← if arg.endsWith ".olean" then
-          let some mod ← searchModuleNameOfFileName arg sp
-            | throw <| IO.userError s!"Could not resolve file name: {arg}"
-          pure mod
-        else
-          match arg.toName with
-          | .anonymous => throw <| IO.userError s!"Not a module name: {arg}"
-          | m => pure m
-        checkMod mod
-    | _, _ => throw <| IO.userError "This does not take flags yet"
+  if let .some bad := flags.find? fun f => !#["--module-list"].contains f then
+    throw <| IO.userError s!"Invalid flag {bad}"
+  args.forM fun arg => do
+    let mod ← if arg.endsWith ".olean" then
+      let some mod ← searchModuleNameOfFileName arg sp
+        | throw <| IO.userError s!"Could not resolve file name: {arg}"
+      pure mod
+    else
+      match arg.toName with
+      | .anonymous => throw <| IO.userError s!"Not a module name: {arg}"
+      | m => pure m
+    withMod mod fun cis => do
+      if flags.contains "--module-list" then
+        let interesting := cis.any fun ci =>
+          match ci with
+          | .defnInfo {value := e, ..}
+          | .thmInfo {value := e,..} =>
+            hasInterestingFunction e
+          | _ => false
+        if interesting then IO.println mod
+      else
+        IO.println s!"Processing {mod}"
+        checkConstInfos cis
   return 0
