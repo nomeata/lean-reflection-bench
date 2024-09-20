8ddd6ae60056b8ee939e3db35a7fb732c6bdbbec
Joachim Breitner <mail@joachim-breitner.de>
2024-09-20 16:28:01 +0200

    Use lean4lean, faster readback, other fixes

--- ReflectionBench/BenchOLean.lean
+++ ReflectionBench/BenchOLean.lean
@@ -1,6 +1,7 @@
 import Lean
 import ReflectionBench.LazyWHNF
 import ReflectionBench.LazyNbE
+import ReflectionBench.TypeChecker
 
 open Lean Meta
 
@@ -11,35 +12,49 @@ partial def isReflProof (e : Expr) : Bool :=
   | id _ e => isReflProof e
   | _ => false
 
+def kernelWhnf (env : Environment) (lctx : LocalContext) (e : Expr) : MetaM Expr := do
+  Lean.ofExceptKernelException <| Lean.Kernel.whnf env lctx e
+
 def runWhnf (desc : String) (whnf : Environment → LocalContext → Expr → MetaM Expr) (e : Expr) : MetaM Nat:= do
   let startT ← IO.monoMsNow
   try
     let r ← whnf (← Lean.getEnv) (← Lean.getLCtx) e
-    unless r matches .const ``Bool.true [] do
-      throwError "Unexpected result {← ppExpr r}"
+    let endT ← IO.monoMsNow
+    try check r
+    catch ex =>
+      withOptions (pp.universes.set · true) do
+        IO.println f!"{desc} reduced\n{← ppExpr e}\nto type-incorrect\n{← ppExpr r}\n{← ex.toMessageData.format}"
+    let r' ← kernelWhnf (← Lean.getEnv) (← Lean.getLCtx) e
+    unless (← isDefEqGuarded r r') do
+      withOptions (pp.universes.set · true) do
+        IO.println f!"{desc} reduced\n{← ppExpr e}\nto\n{← ppExpr r}\nnot defeq to \n{← ppExpr r'}"
+    let diffT := endT - startT
+    return diffT
   catch ex =>
     let f ← Lean.MessageData.format (Lean.Exception.toMessageData ex)
-    IO.println f!"{desc} failed: {f}"
-  let endT ← IO.monoMsNow
-  let diffT := endT - startT
-  return diffT
+    withOptions (pp.explicit.set · true) do
+      IO.println f!"{desc} failed with {f} at\n{← ppExpr e}"
+      let r' ← Meta.whnf e -- kernelWhnf (← Lean.getEnv) (← Lean.getLCtx) e
+    -- unless r matches .const ``Bool.true [] do
+      IO.println f!"Expected result {← ppExpr r'}"
+    return 0
 
-def kernelWhnf (env : Environment) (lctx : LocalContext) (e : Expr) : MetaM Expr := do
-  Lean.ofExceptKernelException <| Lean.Kernel.whnf env lctx e
+def checkWhnf (e : Expr) : MetaM Unit := do
+    -- IO.println f!"Looking at {← ppExpr e}"
+    let stats ←
+      -- runWhnf "Meta.whnf"   (fun _ _ => whnf) e
+      [ runWhnf "Kernel.whnf" kernelWhnf e
+      , runWhnf "lazyWhnf"    lazyWhnf e
+      -- , runWhnf "lazyNbE"     LazyNbE.lazyNbE e
+      ].mapM id
+    if stats.any (· > 5) then
+      IO.println f!"Looking at {← ppExpr e}"
+      IO.println f!"Stats: {stats}"
 
 def checkDecide (p inst eq : Expr) : MetaM Unit := do
   if isReflProof eq then
     let e' := mkApp2 (.const ``Decidable.decide []) p inst
-    -- IO.println f!"Looking at {← ppExpr p}"
-    let stats ←
-      [ runWhnf "Meta.whnf"   (fun _ _ => whnf) e'
-      , runWhnf "Kernel.whnf" kernelWhnf e'
-      , runWhnf "lazyWhnf"    lazyWhnf e'
-      , runWhnf "lazyNbE"     LazyNbE.lazyNbE e'
-      ].mapM id
-    if stats.any (· > 5) then
-      IO.println f!"Looking at {← ppExpr p}"
-      IO.println f!"Stats: {stats}"
+    checkWhnf e'
   else
       IO.println f!"ignoring proof {← ppExpr eq}"
 
@@ -58,10 +73,56 @@ def checkBody (e : Expr) : MetaM Unit := do
       return .continue
   return
 
+def whnfHook (e : Expr) : TypeChecker.M Unit := do
+  unless e.isSort || e.isLambda || e.isLit || e.isForall do
+  -- don't run if already cached
+  if let some _ := (← get).whnfCache[e]? then return
+  let env ← getEnv
+  let lctx ← getLCtx
+  let ctx := {fileName := "mFile", fileMap := Inhabited.default}
+  let state := {env}
+  let mctxt := {lctx}
+  let _ ← (checkWhnf e).run' mctxt |>.toIO ctx state
+
+unsafe def methodsImpl : TypeChecker.Methods where
+  isDefEqCore := fun t s => TypeChecker.Inner.isDefEqCore' t s methodsImpl
+  whnfCore := fun e r p => TypeChecker.Inner.whnfCore' e r p methodsImpl
+  whnf := fun e => do TypeChecker.Inner.whnf' e methodsImpl
+  inferType := fun e i => TypeChecker.Inner.inferType' e i methodsImpl
+
+@[implemented_by methodsImpl]
+opaque methods : TypeChecker.Methods := TypeChecker.Methods.withFuel 0
+
+unsafe def wrappedMethodsImpl : TypeChecker.Methods where
+  isDefEqCore := fun t s => TypeChecker.Inner.isDefEqCore' t s wrappedMethodsImpl
+  whnfCore := fun e r p => TypeChecker.Inner.whnfCore' e r p wrappedMethodsImpl
+  whnf := fun e => do whnfHook e; TypeChecker.Inner.whnf' e methods
+  inferType := fun e i => TypeChecker.Inner.inferType' e i wrappedMethodsImpl
+
+@[implemented_by wrappedMethodsImpl]
+opaque wrappedMethods : TypeChecker.Methods := TypeChecker.Methods.withFuel 0
+
+
+def checkWithLean4Lean (e : Expr) (lps : List Name) : MetaM Unit := do
+  let env ← getEnv
+  let r ← TypeChecker.M.run env .safe {} do
+    withReader ({ · with lparams := lps }) do
+      ReaderT.run (r := wrappedMethods) do
+          TypeChecker.Inner.inferType e (inferOnly := false)
+  match r with
+  | .ok _t => pure () -- IO.println s!"{t}"
+  | .error e => throwError "Lean4Lean complains: {e.toMessageData (← getOptions)}"
+
+
 def checkConstInfo (ci : ConstantInfo) : MetaM Unit := do
   match ci with
-  | .defnInfo ci => checkBody ci.value
-  | .thmInfo ci => checkBody ci.value
+  | .defnInfo ci =>
+    if ci.safety matches .safe then
+      -- checkBody ci.value
+      checkWithLean4Lean ci.value ci.levelParams
+  | .thmInfo ci =>
+      -- checkBody ci.value
+      checkWithLean4Lean ci.value ci.levelParams
   | _ => return
 
 def checkConstInfos (consts : Array ConstantInfo) : MetaM Unit := do
--- ReflectionBench/LazyWHNF.lean
+++ ReflectionBench/LazyWHNF.lean
@@ -22,7 +22,7 @@ abbrev LMap := Array (List Name × List Level)
 
 def Lean.Expr.instLMap (e : Expr) (lmap : LMap) :=
   -- TODO: foldl or foldr
-  lmap.foldl (fun e (us, ls) => e.instantiateLevelParams us ls) e
+  lmap.foldr (fun (us, ls) e => e.instantiateLevelParams us ls) e
 
 inductive StackElem
   | app : Nat → StackElem
@@ -50,19 +50,19 @@ inductive Val
   | pap : ConstantInfo → List Level → LMap → (arity : Nat) → Array Nat → Val
   -- Fully applied constructor
   | con : Name → List Level → LMap → (params fields : Array Nat) → Val
-  -- | vlam : Nat → Val → Val
   | elam : Name → Expr → Expr → LMap → BinderInfo → Val
   | lit : Lean.Literal → Val
   | primNat : Name → Option Nat → Val
+  | neutral : Expr → LMap → Val
 
 def Val.toString : Val → String
   | .pap ci _ _ _ as => s!"{ci.name} {as}"
   | .con cn _ _ ps as => s!"{cn} {ps ++ as}"
-  -- | .vlam n v => s!"λ^{n} ({v.toString})"
   | .elam n _ e _ _ => s!"λ {n}. {e}"
   | .lit l => (repr l).pretty
   | .primNat n .none => s!"{n}"
   | .primNat n (.some m) => s!"{n} {m}"
+  | .neutral e _ => s!"{e}"
 
 instance : ToString Val where toString := Val.toString
 
@@ -71,16 +71,23 @@ inductive HeapElem
   | value : Val → HeapElem
 deriving Inhabited
 
+instance : ToString HeapElem where toString
+  | .thunk e _ => s!"{e}"
+  | .value v => s!"{v}"
+
 abbrev Heap := Array (HeapElem × Env)
 
 def Lean.ConstructorVal.arity (ci : ConstructorVal) : Nat :=
   ci.numParams + ci.numFields
 
+def Lean.InductiveVal.arity (ii : InductiveVal) : Nat :=
+  ii.numParams + ii.numIndices
+
 -- Arity with the major premise
 def Lean.RecursorVal.arity (ci : RecursorVal) : Nat :=
   ci.numParams + ci.numMotives + ci.numMinors + ci.numIndices + 1
 
-partial def toVal (genv : Environment) (lmap : LMap) : Expr → MetaM (Option Val)
+partial def toVal (genv : Environment) (lmap : LMap) (e : Expr) : MetaM (Option Val) := match e with
   | .lam n t e bi => do
     return .some (.elam n t e lmap bi)
   | .const n us => do
@@ -98,60 +105,87 @@ partial def toVal (genv : Environment) (lmap : LMap) : Expr → MetaM (Option Va
       | .quotInfo qi =>
         let arity := match qi.kind with | .type => 2 | .ctor => 3 | .lift => 6 | .ind => 5
         return .some (.pap ci us lmap arity #[])
+      | .inductInfo ii =>
+        if ii.arity = 0 then
+          return .some (.con ci.name us lmap #[] #[])
+        else
+          return .some (.pap ci us lmap ii.arity #[])
       | _ => throwError "Unsupported constant info for {n}"
   | .lit l =>
     return .some (.lit l)
+  | .sort .. | .forallE .. =>
+    return .some (.neutral e lmap)
   | _ => return none
 
+abbrev ReadBackM := ReaderT Heap <| StateT (Array Expr) <| IO
+
+def readBackDummy : Expr := .sort 42
+
 mutual
-partial def ofVal (heap : Heap) (v : Val) (env : Env) : MetaM Expr := do match v with
+
+partial def readBackVal (v : Val) (env : Env) : ReadBackM Expr := do match v with
   | .con cn us lmap ps args =>
-    return mkAppN (Expr.const cn us |>.instLMap lmap) (← (ps ++ args).mapM (ofPos heap ·))
+    return mkAppN (Expr.const cn us |>.instLMap lmap) (← (ps ++ args).mapM readBackPtr)
   | .pap ci us lmap _ args =>
-    return mkAppN (Expr.const ci.name us |>.instLMap lmap) (← args.mapM (ofPos heap ·))
+    return mkAppN (Expr.const ci.name us |>.instLMap lmap) (← args.mapM readBackPtr)
   | .elam n t b lmap bi =>
     let e := Expr.lam n t b bi
     let e := e.instLMap lmap
-    let e := e.instantiate (← env.toArray.mapM (ofPos heap ·))
+    let e := e.instantiate (← env.toArray.mapM readBackPtr)
+    -- unless e.hasLooseBVars do
+    --   IO.eprintln s!"loose bvars: {e}"
     return e
   | .lit t => return .lit t
   | .primNat n none => return .const n []
   | .primNat n (some m) => return mkApp (.const n []) (.lit (.natVal m)) -- ofNat?
+  | .neutral e lmap =>
+    let e := e.instLMap lmap
+    let e := e.instantiate (← env.toArray.mapM readBackPtr)
+    return e
 
+partial def readBackPtr (p : Nat) : ReadBackM Expr := do
+  let eheap ← get
+  let e' := eheap[p]!
+  if e' == readBackDummy then
+    let (he, env) := (← read)[p]!
+    let e ←
+      match he with
+      | .thunk e lmap => pure (e |>.instLMap lmap |>.instantiate (← env.toArray.mapM readBackPtr))
+      | .value v => readBackVal v env
+    assert! e != readBackDummy
+    modify (·.set! p e)
+    return e
+  else
+    return e'
 
-partial def ofPos (heap : Heap) (p : Nat) : MetaM Expr := do
-  let (he, env) := heap[p]!
-  match he with
-  | .thunk e lmap => return e |>.instLMap lmap |>.instantiate (← env.toArray.mapM (ofPos heap ·))
-  | .value v => ofVal heap v env
 end
 
-def inStackElem (heap : Heap) (se : StackElem) (e : Expr) : MetaM Expr := match se with
+def readBackStackElem (se : StackElem) (e : Expr) : ReadBackM Expr := match se with
   | .upd _ => return e
-  | .app p => return mkApp e (← ofPos heap p)
+  | .app p => return mkApp e (← readBackPtr p)
   | .rec_ ci us lmap args =>
-    return mkAppN (Expr.const ci.name us |>.instLMap lmap) ((← args.mapM (ofPos heap ·)) ++ #[e])
+    return mkAppN (Expr.const ci.name us |>.instLMap lmap) ((← args.mapM readBackPtr) ++ #[e])
   | .proj n i => return e.proj n i
   | .nfNat n => return mkNatAdd e (mkNatLit n)
   | .primNat n none => return mkApp (.const n []) e
   | .primNat n (some m) => return mkApp2 (.const n []) (.lit (.natVal m)) e -- ofNat?
 
-def inStack (heap : Heap) (e : Expr) (stack : Stack) : MetaM Expr :=
-  stack.foldrM (inStackElem heap) e
+def readBackStack (e : Expr) (stack : Stack) : ReadBackM Expr :=
+  stack.foldrM readBackStackElem e
 
-def ofConf (heap : Heap) (v : Val) (env : Env) (stack : Stack) : MetaM Expr := do
-  inStack heap (← ofVal heap v env) stack
+def ReadBackM.run (heap : Heap) (act : ReadBackM α) : IO α := do
+  Prod.fst <$> (ReaderT.run act heap).run (mkArray heap.size readBackDummy)
 
-def checkValConf (lctx : LocalContext) (heap : Heap) (v : Val) (env : Env) (stack : Stack) : MetaM Unit := do
+def ofVal (heap : Heap) (v : Val) (env : Env) : IO Expr := ReadBackM.run heap (readBackVal v env)
+def ofPos (heap : Heap) (p : Nat) : IO Expr := ReadBackM.run heap (readBackPtr p)
+def ofConf (heap : Heap) (v : Val) (env : Env) (stack : Stack) : MetaM Expr := ReadBackM.run heap do
+  readBackStack (← readBackVal v env) stack
+
+def checkValConf (heap : Heap) (v : Val) (env : Env) (stack : Stack) : MetaM Unit := do
   let le ← ofConf heap v env stack
-  unless (← withCurrHeartbeats (Meta.withLCtx lctx {} (Meta.isTypeCorrect le))) do
-    IO.eprint s!"In stepVal {le} not typecorrect\n"
-    Meta.check le
-
-def checkExprConf (lctx : LocalContext) (heap : Heap) (e : Expr) (env : Env) (stack : Stack) : MetaM Unit := do
-  let le ← inStack heap (e.instantiate (← env.toArray.mapM (ofPos heap ·))) stack
-  unless (← withCurrHeartbeats (Meta.withLCtx lctx {} (Meta.isTypeCorrect le))) do
-    IO.eprint s!"In stepExpr {le} not typecorrect\n"
+  -- IO.eprint s!"Evaluating:\n{le}\nstack: {stack}\nheap: {heap}\n"
+  unless (← withCurrHeartbeats (Meta.isTypeCorrect le)) do
+    -- IO.eprint s!"Not typecorrect:\n{le}\nstack: {stack}\nheap: {heap}\n"
     Meta.check le
 
 def Val.ofNat (n : Nat) : Val := .lit (.natVal n)
@@ -224,19 +258,21 @@ where
     let (he, env') := heap[p]!
     match he with
     | .thunk e lm =>
-      let heap' := heap.set! p default -- blackholing
+      -- Cannot blackhole if we want to be able abort anytime (neutral values)
+      -- let heap' := heap.set! p default
       let stack' := stack.push (.upd p)
-      goExp heap' e lm env' stack'
+      goExp heap e lm env' stack'
     | .value v =>
       goVal heap v env' stack
 
   | .value =>
     let v : Val := x1
+    -- IO.eprint s!"⊢ {v} (stack empty? {stack.isEmpty})\n"
 
-    if stack.isEmpty then
-      ofVal heap v env
+    if stack.isEmpty || v matches .neutral .. then
+      ofConf heap v env stack
     else
-      -- checkValConf lctx heap v env stack
+      -- checkValConf heap v env stack
 
       let se := stack.back
       let stack := stack.pop
@@ -260,14 +296,18 @@ where
               let ps := args'[:ci.numParams]
               let fs := args'[ci.numParams:]
               goVal heap (.con ci.name us lmap ps fs) env stack
+            | .inductInfo ii =>
+              let args' := args.push p
+              let ps := args'[:ii.numParams]
+              let fs := args'[ii.numParams:]
+              goVal heap (.con ci.name us lmap ps fs) env stack
             | .quotInfo {kind := .ctor, ..} =>
               assert! arity = 3
               goVal heap (.con ci.name us lmap args #[p]) env stack
+            -- This hack enables cheap “Rule K” support. Unsound without checking the types, though!
             /-
-            This enables cheap “Rule K” support. Unsound without checking the types, though!
             | .recInfo {name := ``Eq.rec,..} =>
-              -- Hack: Make Eq.rec ignore the argument
-              go heap (.ind args[3]!) env stack
+              goPtr heap args[3]! stack
             -/
             | _ =>
               -- all other .paps are strict in their last argument, so evaluate that
@@ -284,7 +324,7 @@ where
             goPtr heap p stack
           else
             throwError "Projection out of range"
-        | _ => throwError "Cannot project value"
+        | _ => throwError "Cannot project value {v}"
       | .rec_ ci us lmap args =>
         match ci with
         | .recInfo ri =>
@@ -314,7 +354,7 @@ where
             else
               let rargs : Array Nat := args[:ri.numParams + ri.numMotives + ri.numMinors] ++ fs
               let rhs := rule.rhs
-                let lmap := lmap.push (ri.levelParams, us)
+              let lmap := lmap.push (ri.levelParams, us)
               -- let rhs := rule.rhs.instantiateLevelParams ri.levelParams us
               -- IO.eprint s!"Applying {ri.name} with args {rargs}\n"
               goExp heap rhs lmap [] (stack ++ rargs.reverse.map (.app ·))
@@ -368,7 +408,7 @@ where
         if let some p := env[i]? then
           goPtr heap p stack
         else
-          throwError "Bound variable {i} not supported yet (env: {env})"
+          throwError "Bound variable {i} out of scope"
       | .letE _n _t v b _ =>
         let p := heap.size
         let heap' := heap.push (.thunk v lmap, env)
@@ -379,7 +419,7 @@ where
           if let some p := env[i]? then
             goExp heap f lmap env (stack.push (.app p))
           else
-            throwError "Bound variable {i} not supported yet (env: {env})"
+            throwError "Bound variable {i} out of scope"
         else
           let p := heap.size
           let heap' := heap.push (.thunk a lmap, env)
@@ -409,16 +449,23 @@ where
             let lmap := lmap.push (ci.levelParams, us)
             goExp heap val lmap [] stack
           | _ => throwError "Unimplemented constant: {e}"
+      | .fvar n => do
+        match (← n.getDecl) with
+        | .ldecl _ _ _ _ value _ _ =>
+          goExp heap value lmap [] stack
+        | _ =>
+          goVal heap (.neutral e lmap) env stack
       | .lam .. => unreachable!
       | .mdata _ e => goExp heap e lmap env stack
       | _ => throwError "Unimplemented: {toString e}"
 
 elab "#lazy_reduce" t:term : command => Lean.Elab.Command.runTermElabM fun _ => do
   let e ← Lean.Elab.Term.elabTermAndSynthesize t none
-  let e'' ← lazyWhnf (← Lean.getEnv) (← Lean.getLCtx) e
-  -- Meta.check e''
-  -- guard (← Meta.isDefEq e e'')
-  Lean.logInfo m!"{e''}"
+  Meta.lambdaTelescope e fun _ e => do
+    let e'' ← lazyWhnf (← Lean.getEnv) (← Lean.getLCtx) e
+    -- Meta.check e''
+    -- guard (← Meta.isDefEq e e'')
+    Lean.logInfo m!"{e''}"
 
 set_option linter.unusedVariables false
 set_option pp.funBinderTypes true
@@ -489,3 +536,21 @@ opaque aNat : Nat
 /-- error: Unsupported constant info for aNat -/
 #guard_msgs in
 #lazy_reduce Nat.add (Nat.succ 42) (Nat.succ aNat)
+
+universe u uu
+
+set_option pp.universes true in
+/-- info: List.cons.{uu} c (List.cons.{uu} b (List.cons.{uu} a List.nil.{uu})) -/
+#guard_msgs in
+#lazy_reduce fun (α : Type uu) (a b c : α) => List.reverse [a,b,c]
+
+namespace Levels
+def foo1.{u1} := List.{u1+1}
+def foo2.{u2,u3} := foo1.{max u2 u3}
+
+set_option pp.universes true in
+/-- info: List.{(max uu u) + 1} -/
+#guard_msgs in
+#lazy_reduce foo2.{uu,u}
+
+end Levels
