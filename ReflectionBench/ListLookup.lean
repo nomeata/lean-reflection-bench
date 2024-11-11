625aa12171865bcaba7ca912caa923ffac9b1fc9
Joachim Breitner <mail@joachim-breitner.de>
2024-11-11 14:53:37 +0100

    Experiment with trace-producing computation

--- ReflectionBench/ListLookup.lean
+++ ReflectionBench/ListLookup.lean
@@ -18,6 +18,8 @@ theorem list_head {l : List Nat} {a} : (a::l).get? 0 = some a := rfl
 theorem list_tail {l : List Nat} {n a b} (h : l.get? n = some a) :
   (b::l).get? n.succ = some a := h ▸ rfl
 
+section MariosExample
+
 open Qq
 
 def proveList (n : Nat) (l : Q(List Nat)) : MetaM ((a : Q(Nat)) × Q(($l).get? $n = some $a)) := do
@@ -57,16 +59,107 @@ elab "prove_list_tac" : tactic =>
 
 set_option maxRecDepth 2000
 
+#time
+def test_decide : ∀ n < 2^exp, list.get? n = Option.some n := by decide
+-- 3.601 s
+
+#time
+def test_native_decide : ∀ n < 2^exp, list.get? n = Option.some n := by native_decide
+-- 20ms
+
+#time
+def testM2 : ∀ n < 2^exp, list.get? n = Option.some n := by unfold list; prove_list_tac
+
+-- #print testM2
+
+end MariosExample
+
+
 /-
-set_option trace.profiler true in
-def test1 : ∀ n < 2^exp, list.get? n = Option.some n := by decide
--- 3.301 s
-
-set_option trace.profiler true in
-def test2 : ∀ n < 2^exp, list.get? n = Option.some n := by unfold list; prove_list_tac
--- 0.928 s
-
+A variant that could (hopefully) be automated.
 -/
+
+/-
+def get? : (as : List α) → (i : Nat) → Option α
+  | a::_,  0   => some a
+  | _::as, n+1 => get? as n
+  | _,     _   => none
+-/
+
+-- An aux defintion to avoid nesting an app in an eq, but does not seem to make a big difference
+def List.get?_eq α as i r := @List.get? α as i = r
+
+theorem List.get?_eq_nil : List.get?_eq α [] n none := rfl
+theorem List.get?_eq_cons_zero : List.get?_eq α (a::l) 0 (some a) := rfl
+-- NB: Writing n.succ instead of n+1 here makes a big difference (22% improvement)
+theorem List.get?_eq_cons_succ α a l n r :
+  get? l n = r → @get? α (a::l) n.succ = r :=
+    fun h => Eq.trans (@get?_cons_succ α a l n) h
+
+def List.get?' (u : Level) (αE asE : Expr) : (as : List α) → (i : Nat) → (Option α × Expr × Expr)
+  | a::_,  0  =>
+    (some a,
+    mkApp2 (.const ``Option.some [u]) αE asE.appFn!.appArg!,
+    mkApp3 (.const ``get?_eq_cons_zero [u]) αE asE.appFn!.appArg! asE.appArg!)
+  | _::as, n+1 =>
+    let (r, rE, p) := get?' u αE asE.appArg! as n
+    -- Using mkRawNatLit also important, 18%
+    (r, rE, mkApp6 (.const ``List.get?_eq_cons_succ [u]) αE asE.appFn!.appArg! asE.appArg! (mkRawNatLit n) rE p)
+  | [],     n   =>
+    (none,
+    mkApp (.const ``Option.none [u]) αE,
+    mkApp2 (.const ``get?_eq_nil [u]) αE (mkRawNatLit n))
+
+def Nat.forall_upto (P : Nat → Prop) : Nat → Prop
+  | 0 => True
+  | n+1 => P n ∧ Nat.forall_upto P n
+
+theorem Nat.forall_upto_zero P : Nat.forall_upto P 0 := True.intro
+theorem Nat.forall_upto_succ P n (hP : P n) (h2 : Nat.forall_upto P n) :
+  Nat.forall_upto P (n.succ) := And.intro hP h2
+
+def Nat.forall_upto' (PE : Expr) (mkPE : Nat → Expr) : Nat → Expr
+  | 0 => mkApp (.const ``Nat.forall_upto_zero []) PE
+  | n+1 =>
+    let h1 := mkPE n
+    let h2 := Nat.forall_upto' PE mkPE n
+    mkApp4 (.const ``Nat.forall_upto_succ []) PE (mkNatLit n) h1 h2
+
+def testUpto (n : Nat) : Prop := Nat.forall_upto (fun i => list.get? i = Option.some i) n
+
+def testUpto_intro (n : Nat)
+  (h : Nat.forall_upto (fun i => list.get? i = Option.some i) n) :
+  (testUpto n) := h
+
+
+def testUpto' (n : Nat): Expr :=
+  let natE := mkConst ``Nat
+  -- let listE := mkConst ``list
+  let listVE := toExpr list
+  let PE := mkLambda `i .default natE <|
+    mkApp3 (mkConst ``Eq [1])
+      (mkApp (.const ``Option [0]) natE)
+      (mkApp3 (.const ``List.get? [0]) natE listVE (.bvar 0))
+      (mkApp2 (.const ``Option.some [0]) natE (.bvar 0))
+  let h := Nat.forall_upto' PE (fun i =>
+    let (_, _, p) := List.get?' 0 natE listVE list i
+    p) n
+  mkApp2 (mkConst ``testUpto_intro) (mkNatLit n) h
+  -- h
+
+elab "prove_testUpto" : tactic =>
+  Elab.Tactic.closeMainGoalUsing `test fun t => do
+    let_expr testUpto nE := t | throwError "Unexpected"
+    let n ← unsafe Meta.evalExpr Nat (mkConst ``Nat) nE
+    return testUpto' n
+
+-- set_option diagnostics true
+-- set_option diagnostics.threshold 0
+-- set_option trace.profiler true in
+#time
+theorem test : testUpto (2^exp) := by prove_testUpto
+
+-- #print test
 
 /-
 -- Could build the same proof object using tactics, but too slow
@@ -78,11 +171,35 @@ def test3 : ∀ n < 2^exp, list.get? n = Option.some n := by
   repeat (first | exact list_head | apply list_tail)
 -/
 
-def test : Bool := decide (∀ n < 2^exp, list.get? n = Option.some n)
+def test2 : Bool := decide (∀ n < 2^exp, list.get? n = Option.some n)
 
-#time #kernel_reduce   test
-#time #kernel_reduce   test
-#time #lazy_reduce test
-#time #lazy_reduce test
-#time #lazy_reduce unfold test
-#time #lazy_reduce unfold test
+
+#time #kernel_reduce   test2
+#time #lazy_reduce test2
+#time #lazy_reduce unfold test2
+
+
+-- This models what’s achievable using the `rsimp` machinery
+
+def Nat.all_below (n : Nat) (P : Nat → Bool) : Bool :=
+  Nat.rec true (fun i ih => P i && ih) n
+
+noncomputable def List.get?_rsimp (as : List α) : Nat → Option α :=
+  as.rec
+    (fun _ => none)
+    (fun a _as ih i => i.rec (some a) (fun n _ => ih n))
+
+noncomputable
+def testRSimpType : Bool :=
+    Nat.all_below (2^exp) fun i =>
+    decide (list.get?_rsimp i = some i)
+
+-- This should be roughly as fast as the following #kernel_reduce
+#time
+def testRSimp : testRSimpType := by
+  run_tac
+    Elab.Tactic.closeMainGoalUsing `foo fun _ =>
+      Meta.mkEqRefl (mkConst ``true)
+
+#time #kernel_reduce testRSimpType
+#time #lazy_reduce testRSimpType
